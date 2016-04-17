00cd0ab24f662cf223678eef8960c1211393edbd
Ignas <ignas.sakal@outlook.com>
2016-04-17 19:20:15 +0100

    First commit.

---
+++ src/ServiceContracts/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ServiceContracts")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ServiceContracts")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("36b5da2a-ec40-4a28-b55e-cf49aa5ae9c5")]
---
+++ src/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNet.Mvc;
+using ServiceContracts;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ITestService _testService;
+
+        public HomeController(ITestService testService)
+        {
+            _testService = testService;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["Message"] = _testService.TestMethod();
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
